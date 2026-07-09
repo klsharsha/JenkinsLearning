2a1ea9cfc95d18fe91a1f44c8cbcf0615721270d
klsharsha <lsp5203@psu.edu>
2026-07-09 21:32:39 +0530

    Initial Express Jenkins project

---
+++ src/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+
+app.get("/", (req,res)=>{
+
+    res.json({
+        message:"Jenkins Express Backend Running"
+    });
+
+});
+
+
+app.get("/health",(req,res)=>{
+
+    res.json({
+        status:"UP"
+    });
+
+});
+
+
+app.get("/users",(req,res)=>{
+
+    res.json([
+        {
+            id:1,
+            name:"Harsha"
+        },
+        {
+            id:2,
+            name:"DevOps"
+        }
+    ]);
+
+});
+
+
+module.exports = app;
---
+++ tests/app.test.js
@@ -0,0 +1,70 @@
+const request = require("supertest");
+const app = require("../src/app");
+
+
+describe("Express API Tests",()=>{
+
+
+    test("GET / should return message", async ()=>{
+
+
+        const response =
+            await request(app)
+            .get("/");
+
+
+        expect(response.statusCode)
+        .toBe(200);
+
+
+        expect(response.body.message)
+        .toBe(
+            "Jenkins Express Backend Running"
+        );
+
+
+    });
+
+
+
+    test("GET /health should return UP",async ()=>{
+
+
+        const response =
+            await request(app)
+            .get("/health");
+
+
+        expect(response.statusCode)
+        .toBe(200);
+
+
+        expect(response.body.status)
+        .toBe("UP");
+
+
+    });
+
+
+
+    test("GET /users should return users",async ()=>{
+
+
+        const response =
+            await request(app)
+            .get("/users");
+
+
+        expect(response.statusCode)
+        .toBe(200);
+
+
+        expect(response.body.length)
+        .toBe(2);
+
+
+    });
+
+
+
+});
